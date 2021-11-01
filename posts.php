fc072023cbc00def4d3a591c7ec16f6e9e925a9d
vojinMudrinic <vojin.mudrinic@gmail.com>
2021-11-01 19:44:07 +0100

    Promenjena boja naslova i nav bara

--- posts.php
+++ posts.php
@@ -1,7 +1,7 @@
 <?php
 include "header.php";
-include "footer.php";
 include "side-bar.php";
+
 ?>
 
 
@@ -12,7 +12,7 @@ include "side-bar.php";
         <div class="col-sm-8 blog-main">
 
             <div class="blog-post">
-                <h2 class="blog-post-title">Sample blog post</h2>
+                <h2 class="blog-post-title"><a href = "#">Sample blog post</a></h2>
                 <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
 
                 <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
@@ -45,7 +45,7 @@ include "side-bar.php";
             </div><!-- /.blog-post -->
 
             <div class="blog-post">
-                <h2 class="blog-post-title">Another blog post</h2>
+                <h2 class="blog-post-title"><a href = "#">Another blog post</a></h2>
                 <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
 
                 <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
@@ -57,7 +57,7 @@ include "side-bar.php";
             </div><!-- /.blog-post -->
 
             <div class="blog-post">
-                <h2 class="blog-post-title">New feature</h2>
+                <h2 class="blog-post-title"><a href = "#">New feature</a></h2>
                 <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
 
                 <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
@@ -76,3 +76,4 @@ include "side-bar.php";
             </nav>
 
         </div><!-- /.blog-main -->
+        
